837d20ab637677cceb14d87a2765dbbeb156fbd6
bharath.kv <>
2019-09-13 23:11:25 +0530

    added rank to leaderboard response and integrated an api to clear all sessions

--- apiController/leaderboard.js
+++ apiController/leaderboard.js
@@ -5,10 +5,30 @@ module.exports = (req, res, next) => {
     .then(connector => {
         const dbo = connector.db("vector-db");
         const sortQuery = { totalScore: -1 };
+        const query = { userId: req.query.userId };
+        let response = {
+            "rank": -1,
+            "toppers": []
+        };
+
+        let rank = 0;
+
+        dbo.collection("scores").find().sort(sortQuery).toArray((err, result) => {
+            if(err) throw err;
+            for(let i = 0; i < result.length; i++){
+                if(result[i].userId === req.query.userId){
+                    rank = i+1;
+                    break;
+                }
+            }
+        } )
+
         dbo.collection("scores").find().sort(sortQuery).limit(3).toArray((err, result) => {
             if(err) throw err;
             connector.close();
-            res.send(result);
-        })
+            response.toppers = result;
+            response.rank = rank;
+            res.send(response);
+        });
     })
 }
