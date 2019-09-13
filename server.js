fee7cb5f1a0cb03e45337260587502b59d04db20
prateek <prateek.srivastava@redBus.com>
2019-09-14 02:43:55 +0530

    images

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ app.use(express.static('public'));
 //make way for some custom css, js and images
 app.use('/css', express.static(__dirname + '/public/css'));
 app.use('/js', express.static(__dirname + '/public/js'));
-app.use('/images', express.static(__dirname + '/public/images'));
 app.use('/image', express.static(__dirname + '/public/image'));
 
 app.use('/api/board', function(req, res){
